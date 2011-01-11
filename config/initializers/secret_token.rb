7808d6b376c916c0818ea5411447435abdb7d4bf
Diogo Biazus <diogob@gmail.com>
2011-01-11 12:01:03 -0200

    Rails bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PortalInafag::Application.config.secret_token = '80f5ad912506c45f03974f167e3b621a0d6ef23dd111dbfd3bf09999ea8db364a26e1f8134a6184c6a8a9425f39fe60588d9ca7640fb0e6b72fc5d5706fe6cf3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PortalInafag::Application.config.session_store :cookie_store, :key => '_portal_inafag_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PortalInafag::Application.config.session_store :active_record_store
