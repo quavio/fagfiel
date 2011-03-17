5cbb35ced30110f5f60156ad61413d007b825fb5
Diogo Biazus <diogob@gmail.com>
2011-03-17 16:40:01 -0300

    put goal percentage method and disply goal percentage bar in resellers index

--- spec/views/resellers/index.html.haml_spec.rb
+++ spec/views/resellers/index.html.haml_spec.rb
@@ -3,17 +3,19 @@ require 'spec_helper'
 
 describe "resellers/index.html.haml" do
   context "manager is loged in" do
-    before(:each) do
-      @manager = create_user(:role => "m")
-      @reseller1 = create_reseller
-      @reseller2 = create_reseller(:manager => @manager)
-    end
-
-    it "renders restaurants list with specialties and cuisines" do
-      assign(:resellers, [@reseller1])
-      render
-      assert_select('.reseller', 1) do |r|
-         assert_select('span.ui-label b', 1)
+    [{:goal => 100, :credits => 0}, {:goal => 100, :credits => 100}, {:goal => 100, :credits => 1000}].each do |fields|
+      percentage = (fields[:credits] / fields[:goal] * 100)
+      it "should render the goal percentage #{percentage}" do
+        assign(:resellers, [create_reseller(fields)])
+        render
+        assert_select('.reseller', 1) do |r|
+          assert_select('.progress_bar', 1) do |progress_bar|
+            progress_bar.first.to_s.should match /width: #{percentage > 100 ? 100 : percentage}%/
+          end
+          assert_select('span.ui-label b', 1) do |value|
+            value.first.should match /#{percentage}%/
+          end
+        end
       end
     end
   end
