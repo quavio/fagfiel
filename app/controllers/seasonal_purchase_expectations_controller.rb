4fd030c955c68b9af407c849f0f6604f2c577e2f
Nícolas Iensen <nicolasiensen@Nicolas-Iensens-MacBook.local>
2011-04-01 14:34:23 -0300

    Edit purchase expectation

--- app/controllers/seasonal_purchase_expectations_controller.rb
+++ app/controllers/seasonal_purchase_expectations_controller.rb
@@ -2,21 +2,27 @@ class SeasonalPurchaseExpectationsController < ApplicationController
   before_filter :require_reseller, :only => [:create]
   before_filter :require_same_reseller
   before_filter {@reseller = Reseller.find(params[:reseller_id])}
+  before_filter {@date = Date.new(params[:year].to_i, params[:month].to_i)}
 
   def index
     @purchase_expectations = @reseller.purchase_expectations_for(params[:month], params[:year])
     @seasonal_purchase_expectation = SeasonalPurchaseExpectation.new
-    @date = Date.new(params[:year].to_i, params[:month].to_i)
   end
 
   def create
     product = Product.find_by_reference(params[:product_reference])
-    seasonal_purchase = SeasonalPurchase.find_by_reseller_id_and_product_id_and_month(@reseller.id, product.id, params[:month].to_i)
-    if seasonal_purchase
-    else
-      seasonal_purchase = SeasonalPurchase.create :product_id => product.id, :reseller => @reseller, :month => params[:month]
-      SeasonalPurchaseExpectation.create :seasonal_purchase => seasonal_purchase, :year => params[:year], :quantity => params[:seasonal_purchase_expectation][:quantity]
-    end
+    seasonal_purchase = SeasonalPurchase.find_or_create_by_product_id_and_reseller_id_and_month(product.id, @reseller.id, params[:month])
+    SeasonalPurchaseExpectation.create params[:seasonal_purchase_expectation].merge({:seasonal_purchase => seasonal_purchase, :year => params[:year]})
     redirect_to reseller_seasonal_purchase_expectations_path(@reseller, params[:year], params[:month])
+  end
+
+  def edit
+    @seasonal_purchase_expectation = SeasonalPurchaseExpectation.find(params[:id])
+  end
+
+  def update
+    seasonal_purchase_expectation = SeasonalPurchaseExpectation.find(params[:id])
+    seasonal_purchase_expectation.update_attributes(params[:seasonal_purchase_expectation])
+    redirect_to reseller_seasonal_purchase_expectations_path(@reseller, seasonal_purchase_expectation.year, seasonal_purchase_expectation.seasonal_purchase.month)
   end
 end
