96f1ea97b0d88b95e4199353631b1ed440db8275
Nícolas Iensen <nicolasiensen@Nicolas-Iensens-MacBook.local>
2011-04-13 17:34:28 -0300

    courses_controller refactor

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,61 +1,90 @@
 require 'spec_helper'
 
 describe CoursesController do
-  before(:each) do
-    sign_out :user
-    user = create_user(:role => "r")
-    create_reseller(:user => user)
-    sign_in user
-    @course = create_course :start_at => Time.now + 1.day
+  let(:course){create_course}
+
+  context "admin is signed in" do
+    let(:user){create_user :role => "a"}
+    before(:each){sign_in user}
+
+    describe "GET new" do
+      before(:each){get :new}
+      it{should assign_to(:course).with_kind_of(Course)}
+    end
+
+    describe "GET edit" do
+      before(:each){get :edit, :id => course.id}
+      it{should assign_to(:course).with(course)}
+    end
+
+    describe "GET index" do
+      let(:unactive_course){create_course :start_at => nil}
+      before(:each){get :index}
+      it{should assign_to(:courses).with([course, unactive_course])}
+    end
+
+    describe "POST create" do
+      before(:each){post :create, :course => {'title' => 'new course'}}
+      subject{assigns(:course)}
+      it{should_not be_new_record}
+    end
+
+    describe "PUT update" do
+      before(:each){put :update, :id => course.id, :course => {'title' => 'updated'}}
+      subject{assigns(:course)}
+      its(:title){should be_==('updated')}
+    end
+
+    describe "DELETE destroy" do
+      before(:each){delete :destroy, :id => course.id}
+      subject{Course.find_by_id(course.id)}
+      it{should_not be}
+    end
+
   end
 
-  describe "GET new" do
-    it "assigns a new course as @course" do
-      get :new
-      assigns(:course).id.should be_nil
-    end
-  end
+  ["r", "m"].each do |type|
+    context "#{type} is signed in" do
+      let(:user){create_user :role => type}
+      before(:each){sign_in user}
 
-  describe "GET edit" do
-    it "assigns the requested course as @course" do
-      get :edit, :id => @course.id
-      assigns(:course).id.should eq(@course.id)
-    end
-  end
-
-  describe "GET index" do
-    before(:each){ get :index }
-
-    it "assigns all active courses as @courses" do
-      Set.new(assigns(:courses)).should == Set.new(Course.active.all)
-    end
-    context "when we have courses in the future" do
-      before(:each){ @future = create_course :start_at => Time.now + 1.day }
-      it "assigns all active courses as @courses" do
-        Set.new(assigns(:courses)).should == Set.new(Course.active.all)
+      describe "GET new" do
+        before(:each){get :new}
+        it{should redirect_to(root_path)}
       end
-    end
-  end
 
-  describe "POST create" do
-
-    describe "with valid params" do
-      it "assigns a newly created course as @course" do
-        post :create, :course => {'title' => 'new course'}
-        assigns(:course).id.should be(Course.where(["id <> ?", @course.id]).first.id)
+      describe "GET edit" do
+        before(:each){get :edit, :id => course.id}
+        it{should redirect_to(root_path)}
       end
-    end
 
-  end
-
-  describe "PUT update" do
-
-    describe "with valid params" do
-      it "updates the requested course" do
-        put :update, :id => @course.id, :course => {'title' => 'updated'}
-        @course.reload.title.should == 'updated'
+      describe "GET index" do
+        let(:unactive_course){create_course :start_at => nil}
+        before(:each){get :index}
+        it{should assign_to(:courses).with([course])}
       end
-    end
 
+      describe "GET show" do
+        before(:each){get :show, :id => course.id}
+        it{should assign_to(:course).with(course)}
+      end
+
+      describe "POST create" do
+        before(:each){post :create, :course => {'title' => 'new course'}}
+        it{should redirect_to(root_path)}
+      end
+
+      describe "PUT update" do
+        before(:each){put :update, :id => course.id, :course => {'title' => 'updated'}}
+        it{should redirect_to(root_path)}
+      end
+
+      describe "DELETE destroy" do
+        before(:each){delete :destroy, :id => course.id}
+        it{should redirect_to(root_path)}
+      end
+
+    end
   end
+
 end
