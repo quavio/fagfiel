7e3c62761a4092f4a852f9716a8f86e18512a33f
Nícolas Iensen <nicolasiensen@Nicolas-Iensens-MacBook.local>
2011-04-13 11:42:26 -0300

    courses/new page layout

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,4 @@
 class CoursesController < ApplicationController
   inherit_resources
   actions :all
-  has_scope :active, :type => :boolean, :default => true
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,7 @@
 class Course < ActiveRecord::Base
+  validates_presence_of :title
+  validates_presence_of :price
+
   def self.active
     where('start_at > ?', Date.today)
   end
