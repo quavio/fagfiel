2c600f1e9269762b56b7ed7e6c54c12d1dc2229d
Nícolas Iensen <nicolasiensen@Nicolas-Iensens-MacBook.local>
2011-04-13 11:58:57 -0300

    orders/index layout

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,6 +2,6 @@
 class Order < ActiveRecord::Base
   belongs_to :reseller
   belongs_to :freebie
-  validate {|order| errors.add(:reseller, I18n.t("activerecord.errors.messages.insuficient_found")) if order.freebie.price > order.reseller.balance}
+  validate {|order| errors.add(:reseller, I18n.t("activerecord.errors.messages.insuficient_found")) if order.new_record? && order.freebie.price > order.reseller.balance}
   before_create {|order| order.price = order.freebie.price}
 end
