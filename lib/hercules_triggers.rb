354904fb59f5798fcb79021118823f6175be817d
Nícolas Iensen <nicolasiensen@Nicolas-Iensens-MacBook-Pro.local>
2011-05-02 12:56:14 -0300

    Change deployer to not remove uploaded images

--- lib/hercules_triggers.rb
+++ lib/hercules_triggers.rb
@@ -3,15 +3,9 @@ module Hercules
     def self.before_deploy(options)
       cmd = options[:shell]
       cmd.run "cp config/database.sample.yml config/database.yml"
-      
       cmd.run "mv log/* /home/portal_inafag/logs/"
       cmd.run "rm -rf log"
       cmd.run "ln -s /home/portal_inafag/logs log"
-
-      cmd.run "mv public/system/* /home/portal_inafag/system/"
-      cmd.run "rm -rf public/system"
-      cmd.run "ln -s /home/portal_inafag/system public/system"
-
       cmd.run "bundle exec rake db:migrate RAILS_ENV=#{options[:branch]}"
       cmd.run "bundle exec rake db:seed RAILS_ENV=#{options[:branch]}"
     end
