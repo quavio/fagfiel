4d9097dc0f3b69777ebd6a5c8732244bd9be7ad4
Nícolas Iensen <nicolasiensen@Nicolas-Iensens-MacBook-Pro.local>
2011-05-02 12:45:25 -0300

    Show reseller balance at freebies/index

---
+++ spec/acceptance/show_all_freebies_spec.rb
@@ -0,0 +1,16 @@
+# coding: utf-8
+require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
+
+feature "Show All Freebies" do
+  scenario "Show all as reseller" do
+    user = create_user :role => "r", :password => "123456", :password_confirmation => "123456", :email => "revenda@quavio.com.br"
+    reseller = create_reseller :user => user, :credits => 1000
+    sign_in "revenda@quavio.com.br", "123456"
+
+    click_link "Brindes"
+    
+    within(:css, ".submenu") do
+      page.should have_selector("b", :text => "1000")
+    end    
+  end
+end
